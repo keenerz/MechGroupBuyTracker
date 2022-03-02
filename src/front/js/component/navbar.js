043f59d38eeee2ebe5bb86f044dfa2771918c2ee
Windless95 <barrerajoelj@gmail.com>
2022-03-02 21:31:55 +0000

    simple navbar need to do submission page now

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-gray">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Navbar
+          MechGroupBuyTracker
         </a>
         <button
           class="navbar-toggler"
@@ -21,16 +21,6 @@ export const Navbar = () => {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
             <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
@@ -40,38 +30,9 @@ export const Navbar = () => {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Dropdown
+                Account
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
             </li>
           </ul>
           <form class="d-flex">
@@ -82,7 +43,7 @@ export const Navbar = () => {
               aria-label="Search"
             />
             <button class="btn btn-outline-success" type="submit">
-              Search
+              <i class="fa fa-search"></i>
             </button>
           </form>
         </div>

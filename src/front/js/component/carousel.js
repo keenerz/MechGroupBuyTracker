bb9013294b0235f9692f60ff7bde475a065ed592
Windless95 <barrerajoelj@gmail.com>
2022-03-09 23:24:50 +0000

    added custimization

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -10,46 +10,44 @@ export const frontDisplay = () => {
       setIndex(selectedIndex);
     };
 
-    return (
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.extremetech.com/wp-content/uploads/2019/07/Keystone-with-Silo-Switches.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Keyboard Switches</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://imagehost.vendio.com/a/35122654/aview/QQ20131213011133.jpg"
+          alt="Second slide"
+        />
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
+        <Carousel.Caption>
+          <h3>Keyboard Caps</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://ksassets.timeincuk.net/wp/uploads/sites/54/2018/06/Roccat-Vulcan-100-Hero-1024x683.jpg"
+          alt="Third slide"
+        />
 
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
+        <Carousel.Caption>
+          <h3>Full Keyboards</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>;
   };
 };

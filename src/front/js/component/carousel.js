0f8fb3a13d299a9737cf7d5777edc0f6af07fcc4
Windless95 <barrerajoelj@gmail.com>
2022-03-09 23:20:01 +0000

    carousel almost done?

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -1,96 +1,55 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
+import Carousel from "react-bootstrap/Carousel";
 
 export const frontDisplay = () => {
-  return (
-    <section id="carousel">
-      <div id="carousel-1" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item">
-            <div class="bg-light border rounded border-light front-keyboard carousel-front jumbotron py-5 px-4">
-              <h1 class="front-title">First Slide</h1>
-              <p class="front-subtitle">
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam.
-              </p>
-              <p>
-                <a
-                  class="btn btn-primary front-button plat"
-                  role="button"
-                  href="#"
-                >
-                  Learn more
-                </a>
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="bg-light border rounded border-light front-caps carousel-front jumbotron py-5 px-4">
-              <h1 class="front-title">Second Slide</h1>
-              <p class="front-subtitle">
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam.
-              </p>
-              <p>
-                <a
-                  class="btn btn-primary front-button plat"
-                  role="button"
-                  href="#"
-                >
-                  Learn more
-                </a>
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item active">
-            <div class="bg-light border rounded border-light front-switches carousel-front jumbotron py-5 px-4">
-              <h1 class="front-title">Third Slide </h1>
-              <p class="front-subtitle">
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam.
-              </p>
-              <p>
-                <a
-                  class="btn btn-primary front-button plat"
-                  role="button"
-                  href="#"
-                >
-                  Learn more
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <a
-            class="carousel-control-prev"
-            href="#carousel-1"
-            role="button"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon"></span>
-            <span class="visually-hidden">Previous</span>
-          </a>
-          <a
-            class="carousel-control-next"
-            href="#carousel-1"
-            role="button"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon"></span>
-            <span class="visually-hidden">Next</span>
-          </a>
-        </div>
-        <ol class="carousel-indicators">
-          <li data-bs-target="#carousel-1" data-bs-slide-to="0"></li>
-          <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
-          <li
-            class="active"
-            data-bs-target="#carousel-1"
-            data-bs-slide-to="2"
-          ></li>
-        </ol>
-      </div>
-    </section>
-  );
+  return function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+
+    return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=First slide&bg=373940"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  };
 };

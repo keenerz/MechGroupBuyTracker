55ab7e6f6e01034b1f7bbcc9080aba1fb3f14f83
Windless95 <barrerajoelj@gmail.com>
2022-03-13 16:43:36 +0000

    need to fix listing

--- src/front/js/component/listing.js
+++ src/front/js/component/listing.js
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/listing.css";
 
 export const listing = () => {
+  const { store, actions } = useContext(Context);
+  const [name, setName] = useState("");
+  const [region, setRegion] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
-    <form>
+    <form
+      method="post"
+      onsubmit={(e) => {
+        actions.listing(name, region, price, description).then((session) => {
+          e.preventDefault();
+        });
+      }}
+    >
       <input
+        id="name"
         name="name"
         type="text"
         className="feedback-input"
         placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <input
         name="Region"
         type="text"
         className="feedback-input"
         placeholder="Region"
+        value={region}
+        onChange={(e) => setRegion(e.target.value)}
       />
       <input
         name="Price"
         type="text"
         className="feedback-input"
         placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
       />
       <textarea
         name="text"
         className="feedback-input"
         placeholder="Description of Product"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       ></textarea>
       <input type="submit" value="SUBMIT" />
     </form>

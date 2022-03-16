ff970de37470eb6c60d6602ad57811cd114aa20c
bselket822 <bianca@emergesarasota.org>
2022-03-16 22:22:32 +0000

    new

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,31 +5,24 @@ import "../../styles/home.css";
 import { ProjectList } from "../component/projectlist";
 import { Display } from "../component/carousel";
 
-import logo from "../../img/logo.png";
-import logoGIF from "../../img/logoGIF.gif";
+import homepagegif from "../../img/homepagegif.gif";
 
 export const Home = (props) => {
   const { store, actions } = useContext(Context);
 
   return (
     <div class=" text-center container">
-      <img src={logoGIF} alt="img alt" id="logo-id" />
-      
       <div class="row">
-        <div class="col"> <h1 className=" subhead fw-bold display-1">BUY. <span id="BUY">LIST.</span><br/> <span id="TRACK">TRACK.</span> REPEAT. </h1>
-            <p className="lead text-muted">
-              Unlock some of our newest listed products, while you still can.
-            </p></div>
-        <div class="col"><Display/> </div>
+        <div class="col">
+          <img src={homepagegif} alt="img alt" id="" />
+        </div>
+        <div class="col">
+          <Display />{" "}
+        </div>
       </div>
-      <div>
-        
-      </div>
-
-      
+      <div></div>
 
       <ProjectList />
-     
     </div>
   );
 };

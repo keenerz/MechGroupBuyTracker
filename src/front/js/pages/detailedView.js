83e6f5c2181352b5e401e0beb8be18d29ab25678
Keenan <kkan68@gmail.com>
2022-03-17 22:32:14 +0000

    trying

--- src/front/js/pages/detailedView.js
+++ src/front/js/pages/detailedView.js
@@ -8,9 +8,18 @@ import PropTypes from "prop-types";
 
 export const DetailedView = (props) => {
   const { store, actions } = useContext(Context);
-  const params = useParams().id;
+  const params = useParams();
+  const project = store.projectedit;
+  let project_stage = "";
+  if (project?.project_stage === "interestcheck") {
+    project_stage = "Interest Check";
+  } else if (project?.project_stage === "groupbuy") {
+    project_stage = "Group Buy";
+  } else if (project?.project_stage === "ended") {
+    project_stage = "Ended";
+  }
+  console.log(project);
 
-  const project = store.projects;
   return (
     <div className="card mb-3" style={{ maxWidth: "540px" }}>
       <div className="row g-0">
@@ -23,19 +32,23 @@ export const DetailedView = (props) => {
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">Card title: {props.name}</h5>
-            <p className="card-text">
-              <p className="card-text fw-bold fw-bold">
-                Base Price: {props.price}
-              </p>
-              <p className="card-text fw-bold">
-                Start Date: {props.start_date}
-              </p>
-              <p className="card-text fw-bold">End Date: {props.end_date}</p>
-              <p className="card-text fw-bold">Stage: {props.trackedStatus}</p>
-              <p className="card-text fw-bold">Region: {props.id}</p>
+            <h5 className="card-title">{project?.name}</h5>
+            <p className="card-text fw-bold text-capitalize">
+              Project Type: {project?.project_type}
             </p>
-            <Link to={`/projectedit/${params}`}>
+            <p className="card-text fw-bold text-capitalize">
+              Project Stage: {project_stage}
+            </p>
+            <p className="card-text fw-bold ">
+              Base Price: {project?.baseprice}
+            </p>
+            <p className="card-text fw-bold">
+              Start Date: {project?.start_date}
+            </p>
+            <p className="card-text fw-bold">End Date: {project?.end_date}</p>
+            <p className="card-text fw-bold">Stage: {props.trackedStatus}</p>
+            <p className="card-text fw-bold">Region: {props.id}</p>
+            <Link to={`/projectedit/${params.id}`}>
               <button>edit</button>
             </Link>
             <p className="card-text">
